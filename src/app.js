bb7acaca8935bdf72ea10de8e772540a9f8d8aae
Ocaanaaa <166707320+Ocaanaaa@users.noreply.github.com>
2024-07-01 17:08:17 +0000

    Final version

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector(".btn").addEventListener("click", () => {
+    document.getElementById("excuse").innerText = MainRandom();
+  });
+
+  function MainRandom() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+    function RandomGenerator(list) {
+      return list[Math.floor(Math.random() * list.length)];
+    }
+
+    return (
+      RandomGenerator(who) +
+      " " +
+      RandomGenerator(action) +
+      " " +
+      RandomGenerator(what) +
+      " " +
+      RandomGenerator(when)
+    );
+  }
 };
